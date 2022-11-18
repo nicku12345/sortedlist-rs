65fd6421fa4917c8dee94100e588a145374d59cf
Nick Liu <nicku5011@gmail.com>
2022-11-18 12:40:13 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# SortedList
+
+An implementation of a SortedList data structure in rust.
+
+## Usage
+
+```rust
+let array = vec![90, 19, 25];
+let mut sorted_list = SortedList::from(array);
+
+println!("{:?}", sorted_list);
+// [19, 25, 90]
+
+sorted_list.insert(100);
+sorted_list.insert(1);
+sorted_list.insert(20);
+println!("{:?}", sorted_list);
+// [1, 19, 20, 25, 90, 100]
+
+let x = sorted_list.remove(3);
+println!("{}", x);
+// 25
+// removed the 3-rd smallest (0-indexed) element.
+
+println!("{}", sorted_list.kth_smallest(2));
+// 20
+
+println!("{}", sorted_list[2]);
+// 20
+
+println!("{:?}", sorted_list);
+// [1, 19, 20, 90, 100]
+```
---
+++ src/lib.rs
@@ -0,0 +1,399 @@
+use core::fmt;
+use std::{ops::Index, fmt::Debug};
+
+
+pub struct SortedList<T>
+where T: Ord
+{
+    _lists: Vec<Vec<T>>,
+    _index_tree: Vec<usize>,
+    _index_tree_offset: usize,
+    _load_factor: usize,
+    _upper_load_factor: usize,
+    _lower_load_factor: usize,
+    _len: usize,
+}
+
+impl<T> SortedList<T>
+where T: Ord
+{
+    const DEFAULT_INDEX_TREE_OFFSET: usize = 1<<5;
+    const DEFAULT_LOAD_FACTOR: usize = 5_000;
+    const DEFAULT_UPPER_LOAD_FACTOR: usize = 10_000;
+    const DEFAULT_LOWER_LOAD_FACTOR: usize = 2_500;
+
+    /// Instantiate an empty SortedList.
+    fn _default() -> Self {
+        Self {
+            _lists: vec![],
+            _index_tree: vec![0; 2*Self::DEFAULT_INDEX_TREE_OFFSET],
+            _index_tree_offset: Self::DEFAULT_INDEX_TREE_OFFSET,
+            _load_factor: Self::DEFAULT_LOAD_FACTOR,
+            _upper_load_factor: Self::DEFAULT_UPPER_LOAD_FACTOR,
+            _lower_load_factor: Self::DEFAULT_LOWER_LOAD_FACTOR,
+            _len: 0
+        }
+    }
+
+    /// Collapse self._lists\[i]. self._lists\[i].len() must be > 1.
+    fn _collapse(&mut self, i: usize) {
+        if self._lists.len()<=1 {
+            panic!("Attempting to collapse while self._lists contains only {} lists.", self._lists.len());
+        }
+
+        
+        let left = match i>=1 {
+            true => self._lists[i-1].len(),
+            false => usize::MAX,
+        };
+
+        let right = match i+1<self._lists.len() {
+            true => self._lists[i+1].len(),
+            false => usize::MAX,
+        };
+
+        assert!(left.min(right) < usize::MAX);
+
+        if left<right {
+            // collapse to k-1
+            let mut removed = self._lists.remove(i);
+            self._lists[i-1].append(&mut removed);
+        } else {
+            let mut removed = self._lists.remove(i+1);
+            self._lists[i].append(&mut removed);
+        }
+
+        self._rebuild_index_tree();
+    }
+
+    /// Expand self._lists\[i]. self._lists\[i].len() must be > self._upper_load_factor in order for worthy expansion.
+    fn _expand(&mut self, i: usize) {
+        if self._lists[i].len() < self._upper_load_factor {
+            panic!("Unnecessary expand at self._lists[{}]", i);
+        }
+
+        let size = self._lists[i].len();
+        let removed: Vec<T> = self._lists[i].drain(size/2..).collect();
+        self._lists.insert(i+1, removed);
+
+        // instead of rebuilding the index segment tree, we should check whether we can "shift" the suffix to the right
+        // then update tree at position k and k+1
+        // and rebuild the first half of the index tree
+        self._rebuild_index_tree();
+    }
+
+    /// Rebuild the index segment tree.
+    fn _rebuild_index_tree(&mut self) {
+
+        self._index_tree_offset = Self::DEFAULT_INDEX_TREE_OFFSET; // minimal size lower bound
+        while self._index_tree_offset < self._lists.len() {
+            self._index_tree_offset *= 2;
+        }
+
+        self._index_tree.resize(2*self._index_tree_offset, 0);
+
+        (0..self._lists.len())
+            .for_each(|node| {
+                self._index_tree[node + Self::DEFAULT_INDEX_TREE_OFFSET] = self._lists[node].len();
+
+            });
+
+        (1..Self::DEFAULT_INDEX_TREE_OFFSET)
+            .rev()
+            .for_each(|node| {
+                self._index_tree[node] = self._index_tree[2*node] + self._index_tree[2*node+1];
+            });
+    }
+
+    /// add val to position k of the underlying array of the segment tree
+    fn _index_tree_add(&mut self, i: usize, val: i32) {
+        let mut node = Self::DEFAULT_INDEX_TREE_OFFSET + i;
+        if val>=0 {
+            self._index_tree[node] += val as usize;
+        } else {
+            self._index_tree[node] -= (-val) as usize;
+        }
+        node /= 2;
+
+        while node>0 {
+            self._index_tree[node] = self._index_tree[2*node] + self._index_tree[2*node+1];
+            node /= 2;
+        }
+    }
+
+    /// Remove self._lists\[i]\[j]. It is assumed that self._lists\[i]\[j] will not go out of bound.
+    fn _lists_remove(&mut self, i: usize, j: usize) -> T {
+        if i>=self._lists.len() || j>=self._lists[i].len() {
+            panic!("List index out of range. Attempting to remove self._lists[{}][{}]", i, j);
+        }
+
+        let removed = self._lists[i].remove(j);
+        self._len -= 1;
+
+        if self._lists.len()>1 && self._lists[i].len() < self._lower_load_factor {
+            self._collapse(i);
+        } else {
+            self._index_tree_add(i, -1);
+        }
+
+        return removed;
+    }
+
+    /// Insert `element` into self._lists\[i]. It is assumed that self._lists\[i] is the correct insert position.
+    fn _lists_insert(&mut self, i: usize, element: T) {
+        // insert ele into self._lists[i]
+        // assumptions:
+        // 1. self._lists[i] must exist
+        // 2. i is the correct position for inserting ele
+        let pos =
+            match  self._lists[i].binary_search(&element) {
+                Ok(p) => p,
+                Err(p) => p,
+            };
+
+        self._lists[i].insert(pos, element);
+        self._len += 1;
+        
+        if self._lists[i].len() > self._upper_load_factor {
+            self._expand(i);
+        } else {
+            self._index_tree_add(i, 1);
+        }
+    }
+
+    /// Find the position in self._lists which element should be inserted.
+    fn _bisect_right_lists(&self, element: &T) -> usize {
+        if &self._lists[0][0] > element {
+            return 0;
+        }
+
+        let mut lo = 0;
+        let mut hi = self._lists.len()-1;
+        if &self._lists[hi][0] <= element {
+            return hi;
+        }
+
+        // self._lists[lo][0] <= element
+        // self._lists[hi][0] > element
+        let mut mid;
+        while lo+1 < hi {
+            mid = (lo+hi)/2;
+            if &self._lists[mid][0] <= element {
+                lo = mid;
+            } else {
+                hi = mid;
+            }
+        }
+
+        return lo;
+    }
+
+    /// Returns (i,j) such that self._lists\[i]\[j] is the k-th element (0-indexed) of the SortedList.
+    fn _locate_kth_element(&self, k: usize) -> (usize, usize) {
+        // input k is 0-indexed
+        if k>=self._len {
+            panic!("SortedList: Index out of range.");
+        }
+
+        let is_leaf_node = |u| { u>=self._index_tree_offset };
+        let mut cnt = k+1;
+        
+        let mut node: usize = 1;
+        while !is_leaf_node(node) {
+            if self._index_tree[2*node]>=cnt {
+                node = 2*node;
+            } else {                
+                cnt -= self._index_tree[2*node];
+                node = 2*node+1;
+            }
+        }
+
+        // return values are 0-indexed
+        return (
+            node - self._index_tree_offset,
+            cnt - 1,
+        );
+    }
+
+    /// Retrieve an immutable reference of self._lists\[i]\[j].
+    fn _at(&self, i: usize, j: usize) -> &T {
+        return &self._lists[i][j];
+    }
+
+    /// Returns a flattened view of the SortedList.
+    fn _flat(&self) -> Vec<&T> {
+        self._lists
+            .iter()
+            .fold(Vec::new(), |mut cur, list| {
+                list
+                    .iter()
+                    .for_each(|element| {
+                        cur.push(element);
+                    });
+                cur
+            })
+    }
+}
+
+
+impl<T> SortedList<T>
+where T: Ord
+{
+    /// Find the k-th smallest (0-indexed) element in the SortedList.
+    pub fn kth_smallest(&self, k: usize) -> &T {
+        // k is 0-indexed
+        let (i,j) = self._locate_kth_element(k);
+        return self._at(i, j);
+    }
+
+    /// Insert `element` into the SortedList.
+    pub fn insert(&mut self, element: T) {
+        if self._len==0 {
+            self._lists.push(vec![]);
+            self._lists_insert(0, element);
+            return;
+        }
+
+        let k = self._bisect_right_lists(&element);
+        self._lists_insert(k, element);
+    }
+
+    /// Pops the k-th smallest (0-indexed) element from the SortedList.
+    pub fn remove(&mut self, k: usize) -> T {
+        let (i,j) = self._locate_kth_element(k);
+        return self._lists_remove(i, j);
+    }
+
+}
+
+impl<T> Default for SortedList<T>
+where T: Ord
+{
+    fn default() -> Self {
+        Self::_default()
+    }
+}
+
+impl<T> Index<usize> for SortedList<T>
+where T: Ord
+{
+    type Output = T;
+    fn index(&self, index: usize) -> &Self::Output {
+        self.kth_smallest(index)
+    }
+}
+
+impl<T> From<Vec<T>> for SortedList<T>
+where T: Ord
+{
+    fn from(array: Vec<T>) -> Self {
+        let mut sorted_list = SortedList::default();
+        for x in array {
+            sorted_list.insert(x);
+        }
+        sorted_list
+    }
+}
+
+impl<T> fmt::Debug for SortedList<T>
+where T: Ord + Debug
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(&self._flat(), f)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use rand::{thread_rng, Rng};
+
+    use crate::SortedList;
+
+
+    #[test]
+    fn it_works() {
+        let x = vec![1,2,3,4,9,8,7,6,5,4];
+        let y = vec![1,2,3,4,4,5,6,7,8,9];
+        let arr = SortedList::from(x);
+
+        for i in 0..10 {
+            assert_eq!(y[i], arr[i]);
+        }
+
+    }
+
+    #[test]
+    fn random_tests() {
+        let test_size = 100_000;
+        let test_op = 5_000;
+
+        let mut rng = thread_rng();
+        let mut array = vec![];
+        for _ in 0..test_size {
+            let x = rng.gen::<i32>();
+            array.push(x);
+        }
+
+        // reference sorted list
+        let mut copy = array.clone();
+        copy.sort();
+
+        // actual sorted list
+        let mut sorted_list = SortedList::from(array);
+
+        for _ in 0..test_op {
+            let x = rng.gen::<i32>();
+            
+            // insert into copy
+            let k = match copy.binary_search(&x) {
+                Ok(i) => i,
+                Err(i) => i,
+            };
+            copy.insert(k, x);
+
+
+            // insert into sorted list
+            sorted_list.insert(x);
+        }
+
+        for _ in 0..test_op+test_size {
+            // remove a random index
+            let idx = rng.gen_range(0..copy.len());
+
+            let expected = copy.remove(idx);
+            let actual = sorted_list.remove(idx);
+
+            assert_eq!(expected, actual);
+        }
+    }
+
+    #[test]
+    fn example() {
+        let array = vec![90, 19, 25];
+        let mut sorted_list = SortedList::from(array);
+
+        println!("{:?}", sorted_list);
+        // [19, 25, 90]
+
+        sorted_list.insert(100);
+        sorted_list.insert(1);
+        sorted_list.insert(20);
+        println!("{:?}", sorted_list);
+        // [1, 19, 20, 25, 90, 100]
+
+        let x = sorted_list.remove(3);
+        println!("{}", x);
+        // 25
+        // removed the 3-rd smallest (0-indexed) element.
+
+        println!("{}", sorted_list.kth_smallest(2));
+        // 20
+
+        println!("{}", sorted_list[2]);
+        // 20
+
+        println!("{:?}", sorted_list);
+        // [1, 19, 20, 90, 100]
+    }
+}

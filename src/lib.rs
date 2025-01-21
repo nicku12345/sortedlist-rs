b4081c0fe6b591bef9b8e8cc1e64568f230e2a6e
luffy <luffy.local@gmail.com>
2025-01-20 23:20:53 -0300

    remove useless test code

--- src/lib.rs
+++ src/lib.rs
@@ -887,8 +887,6 @@ mod tests {
     
     #[test]
     fn break_case_insert_after_lst_has_been_clean() {
-        let mut temp = Vec::<usize>::new();
-        temp.clear();
         let mut lst = SortedList::<usize>::new();
         lst.insert(3);
         lst.remove(0);
